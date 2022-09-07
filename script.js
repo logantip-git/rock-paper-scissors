48b18e41544901dc6b834595daae4e2916546821
logantip-git <104089698+logantip-git@users.noreply.github.com>
2022-09-07 11:21:38 -0500

    Add final RPS UI code
    Add UI along with reworked JavaScript and CSS code

--- script.js
+++ script.js
@@ -1,35 +1,102 @@
-function computerPlay() {
-    const play = ['rock', 'paper', 'scissors'];
-    return play[Math.floor(Math.random() * 3)];
+const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+   player: 0,
+   computer: 0
+};
+
+function play(e) {
+   restart.style.display = 'inline-block';
+   const playerChoice = e.target.id;
+   const computerChoice = getComputerChoice();
+   const winner = getWinner(playerChoice, computerChoice);
+   showWinner(winner, computerChoice);
 }
 
-function game() {
-    for (var i = 0; i < 5; i++) {
-
-        function playRound(playerSelection, computerSelection) {
-            if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
-                return "You win! Rock beats Scissors!"
-            } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
-                return "You lose! Paper beats Rock!"
-             } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
-                return "You lose! Rock beats Scissors!"
-             } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
-                return "You win! Scissors beats Paper!"
-             } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
-                return "You win! Paper beats Rock!"
-             } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
-                return "You lose! Scissors beats paper!"
-             } else if (playerSelection.toLowerCase() == computerSelection) {
-                return "Damn it's a tie!"
-             } else {
-                return "Please enter a valid weapon dumbass! Rock, Paper, or Scissors only"
-             }
-    }
-
-    const playerSelection = prompt("Choose your weapon: Rock, Paper, or Scissors?");
-    const computerSelection = computerPlay();
-    console.log(computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
+function getComputerChoice() {
+   const rand = Math.random();
+   if(rand < 0.34) { 
+      return 'rock';
+   } else if(rand <= 0.67) {
+      return 'paper';
+   } else {
+      return 'scissors';
+   }
 }
+
+function getWinner(playerChoice, computerChoice) {
+   if (playerChoice === computerChoice) {
+      return 'draw';
+   } else if(playerChoice === 'rock') {
+      if(computerChoice === 'paper') {
+         return 'computer';
+      } else {
+         return 'player';
+      }
+   } else if (playerChoice === 'paper') {
+      if(computerChoice === 'scissors') {
+         return 'computer';
+      } else {
+         return 'player';
+      }
+   } else if(playerChoice === 'scissors') {
+      if(computerChoice === 'rock') {
+         return 'computer';
+      } else {
+         return 'player';
+      }
+   }
+  
+} 
+
+function showWinner(winner, computerChoice) {
+   if(winner === 'player') {
+      scoreboard.player++;
+      result.innerHTML = `
+      <h1 class="text-win">You Win!</h1>
+      <i class="fas fa-solid fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+      `;
+   } else if(winner === 'computer') {
+      scoreboard.computer++;
+      result.innerHTML = `
+      <h1 class="text-lose">You Lose!</h1>
+      <i class="fas fa-solid fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+      `;
+   } else {
+      result.innerHTML = `
+      <h1>Damn, It's a Tie!</h1>
+      <i class="fas fa-solid fa-hand-${computerChoice} fa-10x"></i>
+      <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>
+      `;
+   }
+   score.innerHTML =`
+   <p>Player: ${scoreboard.player}</p>
+   <p>Computer: ${scoreboard.computer}</p>
+   `;
+
+   modal.style.display = 'block';
 }
-game();
+
+function restartGame() {
+   scoreboard.player = 0;
+   scoreboard.computer = 0;
+   score.innerHTML =`
+   <p>Player: 0</p>
+   <p>Computer: 0</p>
+   `;
+}
+
+function clearModal(e) {
+   if(e.target == modal) {
+      modal.style.display = 'none';
+   }
+}
+
+choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
